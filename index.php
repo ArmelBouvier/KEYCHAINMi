b465e91ac75ae73c4a835f9a8bf0f16b3f6c1968
ArmelBouvier <60919177+ArmelBouvier@users.noreply.github.com>
2020-05-07 16:14:58 +0200

    consignes plus liste des tâches

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+/*KEYCHAINMi
+Créer une application responsive avec une approche mobile first qui permet à l’utilisateur
+de générer des mots de passe aléatoire et complexe et de les lier à une URL.
+Proposer à l’utilisateur de copier son mot de passe.
+Il doit y avoir un espace utilisateur, celui-ci peut s’inscrire pour adhérer au service
+et voir dans son compte la liste des sites avec les mots de passe cachés.
+Quand il clique sur voir le mot de passe, il s’affiche et on lui affiche un
+bouton qui permet de copier le mot de passe.*/
+
+// Etapes :
+// 1 : faire une page d'inscription
+// 2 : faire une page utilisateur
+// 3 : faire un générateur de mot de passe costaud
+// 4 : associer des url aux mots de passe via une db
+// 5 : associer les mdp à un bouton copier
